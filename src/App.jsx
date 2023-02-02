2a169a2162c03d3d0a5b545125029de5b7f9b572
Cee-Jay Malacas <ceejaymalacas17@gmail.com>
2023-02-03 05:30:37 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App text-3xl font-bold underline">
+     <h1>HEHEH</h1>
+    </div>
+  )
+}
+
+export default App
